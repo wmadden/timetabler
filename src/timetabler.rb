f80c2ef388df7c94bc3ba7ce90f49e84406915d3
Will Madden <w.a.madden@gmail.com>
2010-01-30 16:45:23 +1100

    Added sample subjects and parsing.

--- src/timetabler.rb
+++ src/timetabler.rb
@@ -50,7 +50,7 @@ def search( subjects )
     #    end
     #  end
       
-      variables[ [subject, event] ] = streams
+      variables[ [subject, event] ] = streams.values
     end
   end
   
@@ -58,27 +58,27 @@ def search( subjects )
   problem.solve! # I love this line
 end
 
+# Load subject data
+f = File.open( "subjects" )
+lines = f.readlines
+
+subjects = Hash.new { |hash, key| hash[key] = Hash.new() }
+
+for line in lines do
+  re = /\s*([\d]+)\s*([A-Z])(\d+)\/(\d+)\s+([a-zA-Z]+\s+\d+:[\dapm]+ - [\d]+:[\dapm]+)/
+  if re === line
+    subject = $1
+    event = $2
+    event_number = $3
+    stream = $4
+    time = $5
+    #puts "subject = '#{subject}', event = '#{event}', stream = '#{stream}', time = '#{time}'"
+    subjects[ subject ][ event ] ||= {}
+    subjects[ subject ][ event ][ stream ] ||= []
+    subjects[ subject ][ event ][ stream ] << time
+  end
+end
+
 # Test
-puts search({
-  "Computer Design" => {
-    "L1" => [
-      # Stream 1
-      [ [:Monday, 9], [:Wednesday, 9], [:Friday, 9] ],
-      # Stream 2
-      [ [:Monday, 11], [:Wednesday, 11], [:Friday, 11] ],
-      # Stream 2
-      [ [:Monday, 13], [:Wednesday, 13], [:Friday, 13] ]
-    ]
-  },
-  "Data on the Web" => {
-    "L1" => [
-      # Stream 1
-      [ [:Monday, 9], [:Wednesday, 9], [:Friday, 9] ],
-      # Stream 2
-      [ [:Monday, 11], [:Wednesday, 11], [:Friday, 11] ],
-      # Stream 2
-      [ [:Monday, 13], [:Wednesday, 13], [:Friday, 13] ]
-    ]
-  }
-}).inspect
+puts search(subjects).inspect
 
